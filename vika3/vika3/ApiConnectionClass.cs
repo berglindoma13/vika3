5861774716214470185f5bd99760ac55815f7a44
berglindoma13 <berglindoma13@ru.is>
2016-12-12 12:28:00 +0000

    saved

--- vika3/vika3/ApiConnectionClass.cs
+++ vika3/vika3/ApiConnectionClass.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DM.MovieApi;
 
 namespace vika3
 {
-    class ApiConnectionClass
+    public class ApiConnectionClass : IMovieDbSettings
     {
+        public string ApiKey
+        {
+            get { return "933832d8d52af17d616ac7be600e4f8c"; }
+        }
+        public string ApiUrl
+        {
+            get { return "https://api.themoviedb.org/3/"; }
+        }
+
+        public ApiConnectionClass() { }
     }
 }
