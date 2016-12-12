99c32098d724d305fe25d64a415ec0960052171d
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-12-12 12:48:28 +0000

    works

--- vika3/vika3/GreetingPage.cs
+++ vika3/vika3/GreetingPage.cs
@@ -10,35 +10,38 @@ namespace vika3
 {
     public class GreetingPage : ContentPage
     {
-        private Button _searchButton = new Button()
-        {
-            Text = "Get movies",
-            BorderColor = Color.Teal,
+		private Button _searchButton = new Button()
+		{
+			Text = "Get movies",
+			BackgroundColor = Color.Gray,
+			TextColor = Color.White,
             HorizontalOptions = LayoutOptions.Fill
         };
 
         private Label _movieResult = new Label()
         {
-            Text = string.Empty
+            Text = string.Empty,
+			TextColor = Color.White,
         };
 
         private Label _greeting = new Label()
         {
             HorizontalOptions = LayoutOptions.Start,
             HorizontalTextAlignment = TextAlignment.Center,
+			TextColor = Color.White,
             Text = "Enter a word in a movie",
             FontSize = 30
         };
 
-        private Entry _movieSearchEntry = new Entry
-        {
-            HorizontalOptions = LayoutOptions.Fill,
-            Placeholder = "Movie word"
+		private Entry _movieSearchEntry = new Entry
+		{
+			HorizontalOptions = LayoutOptions.Fill,
+			Placeholder = "Movie word",
+			Keyboard = Keyboard.Default
         };
 
         public GreetingPage()
         {
-
             BackgroundColor = Color.Teal;
             Title = "Welcome to Movie Search";
             Content = new StackLayout
