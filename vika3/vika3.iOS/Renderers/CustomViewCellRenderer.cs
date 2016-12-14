debb02f8506e1dc2af3ca86546ad7e7cde186b3f
berglindoma13 <berglindoma13@ru.is>
2016-12-14 10:48:34 +0000

    bla

---
+++ vika3/vika3.iOS/Renderers/CustomViewCellRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using vika3.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(ViewCell), typeof(CustomViewCellRenderer))]
+namespace vika3.iOS.Renderers
+{
+    class CustomViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            cell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+
+            return cell;
+        }
+    }
+}
+
---
+++ vika3/vika3/TopRated.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using DM.MovieApi;
+using Xamarin.Forms;
+
+namespace vika3
+{
+    
+    public class TopRated : ContentPage
+    {
+        private Movies _TopRatedMovies;
+        private ActivityIndicator _progressBar = new ActivityIndicator
+        {
+            Color = Color.White,
+            IsRunning = false
+        };
+
+        public async void TopRatedMovies()
+        {
+            _progressBar.IsRunning = true;
+            //populate list
+            _TopRatedMovies.AllMovies.Clear();
+
+            var movieApi = MovieDbFactory.Create<DM.MovieApi.MovieDb.Movies.IApiMovieRequest>().Value;
+            DM.MovieApi.ApiResponse.ApiSearchResponse<DM.MovieApi.MovieDb.Movies.MovieInfo> response = await movieApi.GetTopRatedAsync();
+
+            if (response != null)
+            {
+                foreach (var i in response.Results)
+                {
+                    var resp = await movieApi.GetCreditsAsync(i.Id);
+                    var response2 = await movieApi.FindByIdAsync(i.Id);
+                    var tmpmovie = new Movie();
+
+                    _TopRatedMovies.AddMovie(i, resp, response2, tmpmovie);
+                }
+            }
+
+
+            _progressBar.IsRunning = false;
+            //Render list as movieListPage
+            this.Navigation.PushAsync(new MovieListPage() { BindingContext = this._TopRatedMovies });
+        }
+       
+    }
+}
