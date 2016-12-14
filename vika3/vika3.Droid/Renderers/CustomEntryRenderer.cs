f703674431a4ab38604ae9baacafb7b9d374e45b
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-12-14 15:33:21 +0000

    b-e-a-utifull

---
+++ vika3/vika3.Droid/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using vika3.Droid.Renderers;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace XFHelloWorld.Droid.Renderers
+{
+    using Xamarin.Forms.Platform.Android;
+
+    using Resource = vika3.Droid.Resource;
+
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (this.Control != null)
+            {
+                var nativeEditText = (EditText)this.Control;
+                nativeEditText.SetBackgroundResource(Resource.Layout.Box);
+            }
+        }
+    }
+}
